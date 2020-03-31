bd45e56350cea45251bf9c226ee1324bcdb4a41b
Diego Ibarra <dibarra@uchicago.edu>
2020-03-30 21:43:06 -0700

    Removed junk

--- env.sh
+++
@@ -1,4 +0,0 @@
-# public key FAHXFUYV
-# private key 941b4d59-12a4-4f93-9144-3c24a99f94c2
-export MONGODB_URI="mongodb+srv://dibarra:wda9rm6w@momentcluster-lsgqv.mongodb.net/Moment?retryWrites=true&w=majority"
-export SECRET="It's gonna be legen... wait for it... dary!"
